f69283c9df5fe0b544c7618d15527aca9095f823
leayl <lea_yl@163.com>
2020-03-26 22:58:14 +0800

    【数据结构与算法】栈和队列 迭代：使用迭代对斐波拉契数列进行二分查找

--- 数据结构与算法小练习/栈和队列/stack_bin2dec.py
+++ 数据结构与算法小练习/栈和队列/stack_bin2dec.py
@@ -104,7 +104,49 @@ def calculate():
     cal(poland_expr)
 
 
+def fibo(num):
+    """
+    斐波拉契数列
+    :param num:需要展示的第num个斐波拉契
+    :return:
+    """
+
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibo(num - 1) + fibo(num - 2)
+
+
+def half_find(l, value, left, right):
+    """
+    使用二分查找法找到数组中指定值的下标
+    :param l: 数组
+    :param value: 要查找位置的值
+    :return: 所查找位置的值
+    """
+    if left > right:
+        return None
+    mid = (left + right) // 2
+    if l[mid] == value:
+        return mid
+    elif l[mid] > value:
+        right = mid - 1
+        return half_find(l, value, left, right)
+    elif l[mid] < value:
+        left = mid + 1
+        return half_find(l, value, left, right)
+
+
 if __name__ == '__main__':
     # bin2dec()
     # cal()
-    calculate()
+    # calculate()
+
+    # 使用迭代对斐波拉契数列进行二分查找
+    l = []
+    for i in range(1, 13):
+        l.append(fibo(i))
+    print(l)
+    print(half_find(l, 21, 0, len(l)))
